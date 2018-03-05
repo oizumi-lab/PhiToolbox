7702af08a3a43c775a584d1f8b28ac2a27c8a902
JunKitazono <kitazono@araya.org>
2018-03-05 21:23:14 +0900

    Changed Input and Output
    Output
    probs: cell -> struct
    
    Input
    params -> tau, number_of_states

--- data_to_probs.m
+++ data_to_probs.m
@@ -1,17 +1,20 @@
-function probs = data_to_probs(type_of_dist,X,params)
+function probs = data_to_probs(type_of_dist, X, tau, number_of_states)
+
+probs.type_of_dist = type_of_dist;
+probs.number_of_elements = size(X, 1);
 
 switch type_of_dist
     case 'Gauss'
-        tau = params(1);
-        [Cov_X, Cov_XY, Cov_Y] = Cov_comp(X,tau);
-        probs{1} = Cov_X;
-        probs{2} = Cov_XY;
-        probs{3} = Cov_Y;
-    case 'dis'
-        tau = params(2);
-        N_st = params(3);
-        [p_past, joint, p_present] = est_prior_joint(X,N_st,tau);
-        probs{1} = p_past;
-        probs{2} = joint;
-        probs{3} = p_present;
+        [probs.Cov_X, probs.Cov_XY, probs.Cov_Y] = Cov_comp(X, tau);
+    case 'discrete'
+        if nargin > 3
+            probs.number_of_states = number_of_states;
+            [probs.past, probs.joint, probs.present] = est_p(X, probs.number_of_states, tau);
+        else
+            error('Number of states must be identified.')
+        end
+    otherwise
+        error('type_of_dist must be ''Guass'' or ''discrete''.')
+end
+
 end
