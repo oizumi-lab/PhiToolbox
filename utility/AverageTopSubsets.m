7beffe9d4aefe52586e408999c7dc4ee3d8e8b0c
JunKitazono <kitazono@araya.org>
2018-04-03 03:13:10 +0900

    Modify codes for averaging complexes

--- utility/AverageTopSubsets.m
+++ utility/AverageTopSubsets.m
@@ -1,4 +1,4 @@
-function [AveragedComplex, AveragedPhi] = AverageComplexes( indices, phis, nelem, numTops )
+function [WeightedRatio, AveragedPhi] = AverageTopSubsets( indices, phis, nelem, numTops )
 % AverageComplexes.m
 %   Calculate weighted average of complexes and phi for the Top (numTops) phi. 
 %
@@ -10,8 +10,8 @@ function [AveragedComplex, AveragedPhi] = AverageComplexes( indices, phis, nelem
 %    numTops: The average is taken for Top (numTops) phi. 
 %
 % OUTPUT:
-%    AveragedComplex: The average of complexes weighted by phi. 1 by nelem
-%    vector.
+%    WeightedRatio: The ratio of the number of times each element is
+%    included in the top subsets to numTops with weightin by phi. 1 by nelem vector.
 %       AveragedComplex(i) = (sum_{k=1}^numTops phi(k) I(The i-th element is included in the k-th subset))/ (sum_{k=1}^numTops phi(k)), 
 %       where I() is the indicator function, and denom = sum_{k=1}^numTops phi(k).
 %
@@ -22,14 +22,14 @@ function [AveragedComplex, AveragedPhi] = AverageComplexes( indices, phis, nelem
 
 [phis_sort, index_phis_sort] = sort(phis, 'descend');
 
-AveragedComplex = zeros(1, nelem);
+WeightedRatio = zeros(1, nelem);
 
 denom = sum(phis_sort(1:numTops));
 for i = 1:numTops
     ind_temp = indices{index_phis_sort(i)};
-    AveragedComplex(ind_temp) = AveragedComplex(ind_temp) + phis_sort(i);
+    WeightedRatio(ind_temp) = WeightedRatio(ind_temp) + phis_sort(i);
 end
-AveragedComplex = AveragedComplex/denom;
+WeightedRatio = WeightedRatio/denom;
 
 AveragedPhi = denom/numTops;
 
