bdd36ce1691564d2ec1e1f546d1e7c4ed81457b0
JunKitazono <kitazono@araya.org>
2019-03-08 20:03:40 +0900

    Delete unnecessary files

--- tools/TotalCorrDecomposition/TotalCorrDecomposition/nancorrcoef.m
+++
@@ -1,5 +0,0 @@
-function [ corr,p ] = nancorrcoef(data)
-%Œ‡‘¹’l(nan)‚ğœ‚¢‚Ä‘ŠŠÖŒW”‚ğŒvZ‚·‚é
-%function [ corr,p ] = nancorrcoef(data)
-nonnan=find(~isnan(sum(data,2)));
-[corr,p]=corrcoef(data(nonnan,:));
