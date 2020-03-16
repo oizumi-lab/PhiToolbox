006c324dcb54a39ce4dcafdc4e61bebd88e3711f
JunKitazono <kitazono@araya.org>
2020-03-17 04:19:56 +0900

    Update plotComplexes.m

--- utility/plotComplexes.m
+++ utility/plotComplexes.m
@@ -55,111 +55,33 @@ colors = cmap(colorIndices,:);
 caxis(cLim)
 
 hold on
-switch plotType
-    case {'BirdsEye', '3D'}
-        G = ones(nNodes);
-        G(1:(nNodes+1):end) = 0;
-        G = graph(G);
-        for iComplexes = 1:nComplexes
-            indices_temp = complexes_sorted{iComplexes};
-            
-            phi_temp = phis_sorted(iComplexes);
-            G_temp = subgraph(G, indices_temp);
-            subGPlotProps_temp = getSubGraphPlotProperties(G, indices_temp, args{:});
-            
-            switch plotType
-                case 'BirdsEye'
-                    ZData_temp = repelem(phi_temp, length(indices_temp))*1e3;
-                case '3D'
-                    ZData_temp = ZData(indices_temp);
-            end
-            plot(G_temp, 'NodeColor', colors(iComplexes,:), ...
-                'EdgeColor', colors(iComplexes,:), ...
-                'XData', XData(indices_temp), 'YData', YData(indices_temp), ...
-                'ZData', ZData_temp, subGPlotProps_temp{:} );
-%             plot(G_temp, 'NodeColor', colors(iComplexes,:), ...
-%                 'EdgeColor', colors(iComplexes,:), ...
-%                  subGPlotProps_temp{:} );
-        end
-        
-    case '2D'
-        G = ones(nNodes);
-        G(1:(nNodes+1):end) = 0;
-        G = graph(G);
-        %adjG = adjacency(G);
-        
-        %H = G;
-        %adjH = adjacency(H);
-        adjG = adjacency(G);
-        adjH = zeros(numnodes(G));
-        
-        allIndices = [];
-        for iComplexes = 1:nComplexes
-            indices_temp = complexes_sorted{iComplexes};
-            allIndices = union(allIndices, indices_temp);
-            
-            adjH(indices_temp, indices_temp) = adjG(indices_temp, indices_temp);
-            
-        end
-        H = graph(adjH);
-        H = subgraph(H, allIndices);
-        
-        subGPlotProps_temp = getSubGraphPlotProperties(G, allIndices, args{:});
-        
-        plotH = plot(H, 'XData', XData(allIndices), 'YData', YData(allIndices), subGPlotProps_temp{:});
-        
-        for iComplexes = 1:nComplexes
-            indices_temp = complexes_sorted{iComplexes};
-            adjH_temp = zeros(numnodes(G));
-            adjH_temp(indices_temp, indices_temp) = adjG(indices_temp, indices_temp);
-            H_temp = graph(adjH_temp);
-            H_temp = subgraph(H_temp, allIndices);
-            
-            highlight(plotH, H_temp, 'NodeColor', colors(iComplexes,:), 'EdgeColor', colors(iComplexes,:) )
-        end
-%         
-%                 
-%         G.Edges.Weight(:) = 0;
-%         for iComplexes = 1:nComplexes
-%             indices_temp = complexes_sorted{iComplexes};
-%             G
-%             
-%         end
-%                 
-%         allIndices = [];
-%         
-%         
-%         NodeCData = zeros(allIndices, 1);
-%         for iComplexes = 1:nComplexes
-%             indices_temp = complexes_sorted{iComplexes};
-%             allIndices = union(allIndices, indices_temp);
-%             
-%             phi_temp = phis_sorted(iComplexes);
-%             G(indices_temp, indices_temp) = phi_temp;
-%             NodeCData(indices_temp) = phi_temp;
-%         end
-%         G(1:(nNodes+1):end) = 0;
-%         G = graph(G);
-%         subG = subgraph(G, allIndices);
-%         
-% %         subG = G(allIndices, allIndices);
-% %         subG = graph(subG);
-%         subGPlotProps_temp = getSubGraphPlotProperties(G, allIndices, args{:});
-%         
-%         NodeCData = my_rescale(NodeCData(allIndices), cLim(1), cLim(2));
-%         EdgeCData = my_rescale(subG.Edges.Weight, cLim(1), cLim(2));
-%         
-%         plot(subG, 'XData', XData(allIndices), 'YData', YData(allIndices), ...
-%         'NodeCData', NodeCData, 'EdgeCData', EdgeCData, ...
-%         subGPlotProps_temp{:});
-% %         plot(subG, ...
-% %         'NodeCData', NodeCData, 'EdgeCData', EdgeCData, ...
-% %         subGPlotProps_temp{:});
+G = ones(nNodes);
+G(1:(nNodes+1):end) = 0;
+G = graph(G);
+for iComplexes = 1:nComplexes
+    indices_temp = complexes_sorted{iComplexes};
+    
+    phi_temp = phis_sorted(iComplexes);
+    G_temp = subgraph(G, indices_temp);
+    subGPlotProps_temp = getSubGraphPlotProperties(G, indices_temp, args{:});
+    
+    switch plotType
+        case '2D'
+            ZData_temp = zeros(length(indices_temp), 1);
+        case 'BirdsEye'
+            ZData_temp = repelem(phi_temp, length(indices_temp));
+        case '3D'
+            ZData_temp = ZData(indices_temp);
+    end
+    plot(G_temp, 'NodeColor', colors(iComplexes,:), ...
+        'EdgeColor', colors(iComplexes,:), ...
+        'XData', XData(indices_temp), 'YData', YData(indices_temp), ...
+        'ZData', ZData_temp, subGPlotProps_temp{:} );
+end
+
         
 end
 
-
-end
 
 
 function phis_rescaled = my_rescale(phis, minPhi, maxPhi)
